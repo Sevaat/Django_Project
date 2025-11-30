a145438b5c3e09d097050f96764b22841f066bf8
tkachenko <sevaatmail@gmail.com>
2025-11-30 13:12:46 +0500

    Исправлено имена полей Product

--- catalog/models.py
+++ catalog/models.py
@@ -18,13 +18,13 @@ class Product(models.Model):
     image = models.ImageField(upload_to='product/photo', blank=True, null=True, verbose_name="Изображение продукта")
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Категория продукта", related_name="products")
     price = models.CharField(max_length=50, verbose_name="Стоимость продукта")
-    production_date = models.DateField(verbose_name="Дата изготовления (создания) продукта")
-    last_modified_date = models.DateTimeField(verbose_name="Дата последнего изменения продукта")
+    created_at = models.DateField(verbose_name="Дата изготовления (создания) продукта")
+    updated_at = models.DateTimeField(verbose_name="Дата последнего изменения продукта")
 
     class Meta:
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
-        ordering = ['name', 'category', 'price', 'production_date', 'last_modified_date']
+        ordering = ['name', 'category', 'price', 'created_at', 'updated_at']
 
     def __str__(self):
         return self.name
