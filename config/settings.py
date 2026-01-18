2b47fe4731614bf3fa1f5ee34b0fc00f3eeb0c47
Tkachenko Vsevolod <sevaatmail@gmail.com>
2026-01-18 18:01:45 +0500

    Подключен Redis

--- config/settings.py
+++ config/settings.py
@@ -153,3 +153,12 @@ EMAIL_USE_SSL = True
 
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+CACHE_ENABLED = True
+if CACHE_ENABLED:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis_cache.RedisCache',
+            'LOCATION': os.getenv('REDIS_LOCATION'),
+        }
+    }
